e0140fbcd7efc75675a35c97c764e81fec4f3f97
Chia Weng Chin <chiawc21.21@ichat.sp.edu.sg>
2022-10-27 17:00:28 +0800

    added category to bmi

--- bmi.py
+++ bmi.py
@@ -1,10 +1,18 @@
 def calculate_bmi(height, weight):
-    print ("Height = " + str(height))
-    print ("Weight = " + str(weight))
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
 
-    #Add code here to calculate BMI
-    bmi = weight/(height**2)
-    print ("BMI = " + str(bmi))
+    # Add code here to calculate BMI
+    bmi = weight / (height ** 2)
+    print("BMI = " + str(bmi))
 
-calculate_bmi(weight = 57, height = 1.73)
+    # Categorize the BMI
+    if bmi < 18.5:
+        print("Underweight!")
+    elif bmi >= 18.5 or bmi <= 25.0:
+        print("Normal Weight!")
+    else:
+        print("Overweight!")
 
+
+calculate_bmi(weight=57, height=1.73)
